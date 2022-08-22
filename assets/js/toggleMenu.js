77e2619f7f70c5690f4f494d41f4f86010b430f8
Lucas <lucaszaquila@gmail.com>
2022-08-22 14:35:34 -0300

    Prettier

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-    document.querySelector('#menu').classList.toggle('active');
-    document.querySelector('html').classList.toggle('active');
+  document.querySelector("#menu").classList.toggle("active");
+  document.querySelector("html").classList.toggle("active");
 }
